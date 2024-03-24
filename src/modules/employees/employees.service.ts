6aa7b4599dc7d1c037a9c50111e299ebc010dcf0
Samuel Airehrour <sairehrour@gmail.com>
2024-03-24 20:37:35 +0100

    chore: removed unnecessary comments

--- src/modules/employees/employees.service.ts
+++ src/modules/employees/employees.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { Prisma, Role } from '@prisma/client';
 import { DatabaseService } from '../database/database.service';
